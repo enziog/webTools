161cbcf308f416103e9e04dda6ab337f6024e32e
EnzioG <enzio.g@qq.com>
2021-10-28 12:00:23 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@ extern crate serde_derive;
 
 mod markdown;
 
-//use std::env;
-//use std::fs::File;
-//use std::io::prelude::*;
 use yew::format::Json;
 use yew::services::storage::Area;
 use yew::services::{DialogService, StorageService};
@@ -37,8 +34,6 @@ pub struct Probe {
 impl Probe {
     fn empty() -> Self {
         Probe {
-            //first_name: "".into(),
-            //last_name: "".into(),
             description: "".into(),
             frequency: 0.0,
             velocity: 0.0,
@@ -233,16 +228,6 @@ impl Component for Model {
                 Msg::UpdateVelocity(val) => {
                     probe.velocity = val;
                 }
-                /*
-                Msg::UpdateFirstName(val) => {
-                    println!("Input: {}", val);
-                    probe.first_name = val;
-                }
-                Msg::UpdateLastName(val) => {
-                    println!("Input: {}", val);
-                    probe.last_name = val;
-                }
-                */
                 Msg::UpdateDescription(val) => {
                     println!("Input: {}", val);
                     probe.description = val;
@@ -487,85 +472,3 @@ impl Probe {
         }
     }
 }
-
-/*
-#![recursion_limit = "4096"]
-
-use yew::{html, Component, ComponentLink, Html, InputData, ShouldRender};
-
-pub struct Model {
-    link: ComponentLink<Self>,
-    //value: String,
-    velocity: f64,
-    vel: f64,
-    frequency: f64,
-    //存储结果
-    lambda: f64,
-}
-
-pub enum Msg {
-    GotVelocity(String),
-    GotFrequency(String),
-    Clicked,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Model {
-            link,
-            velocity: 0.0,
-            frequency: 0.0,
-            //结果存储
-            lambda: 0.0,
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::GotFrequency(new_value) => {
-                self.frequency = new_value.parse().unwrap();
-            }
-            Msg::GotVelocity(new_value) => {
-                self.velocity = new_value.parse().unwrap();
-            }
-            Msg::Clicked => {
-                self.lambda = calc(self.frequency, self.velocity);
-                //println!("波长为： {}mm", self.lambda);
-            }
-        }
-        true
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <div>
-                <div>
-                    <textarea rows=1
-                        value=&self.velocity
-                        oninput=self.link.callback(|f: InputData| Msg::GotVelocity(f.value))
-                        placeholder="声速">
-                    </textarea>
-                    <textarea rows=1
-                        value=&self.frequency
-                        oninput=self.link.callback(|e: InputData| Msg::GotFrequency(e.value))
-                        placeholder="频率">
-                    </textarea>
-                    <button onclick=self.link.callback(|_| Msg::Clicked)>{ "计算" }</button>
-                </div>
-                <div>
-                    { probe.view_vel_input(&self.link)}
-                    {&self.lambda}
-                </div>
-            </div>
-        }
-    }
-}
-
-fn calc(freq: f64, velo: f64) -> f64 {
-    let lambda = velo / 1000.0 / freq;
-    lambda
-}
-*/
